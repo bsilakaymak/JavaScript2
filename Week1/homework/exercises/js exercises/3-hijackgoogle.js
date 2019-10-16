5ae797171ddc974e49714c1f5ac62ae36e591372
Sila <basaksilakaymak@gmail.com>
2019-10-16 02:26:50 +0200

    deleted last weeks files I accidentally added to this week

--- Week1/homework/exercises/js exercises/3-hijackgoogle.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function hijackGoogleLogo(id, source){
-   const elementToBeChanged = document.getElementById(id);
-   elementToBeChanged.src = source;
-   elementToBeChanged.srcset = source;
-}
-
-hijackGoogleLogo("hplogo", "https://www.hackyourfuture.dk/static/logo-dark.svg");
